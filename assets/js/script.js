960364b9fb946ebdf79b2a57b94ca7431db59bce
xstinky <Russo709@gmail.com>
2024-02-01 19:00:49 +0100

    changed theme selection from dropdown menu to toggle button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+  let storedTheme = getStoredTheme();
+  setThemeAttribute(storedTheme);
+  switchTheme()
+});
+
 function getStoredTheme() {
-    return localStorage.getItem("theme") || "light";
+  return localStorage.getItem('theme') || 'light';
 }
 
 function setThemeAttribute(theme) {
-    document.documentElement.setAttribute('data-bs-theme', theme);
-    localStorage.setItem('theme', theme)
+  document.documentElement.setAttribute('data-bs-theme', theme);
+  localStorage.setItem('theme', theme);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    let storedTheme = getStoredTheme();
-    setThemeAttribute(storedTheme);
-})
-
 function setThemeDark() {
-    setThemeAttribute('dark')
+  setThemeAttribute('dark');
 }
 
 function setThemeLight() {
-    setThemeAttribute('light')
+  setThemeAttribute('light');
 }
 
-function dropdownColorBg(hoveredElement) {
-    if(localStorage.getItem('theme')=='light') {
-        hoveredElement.style.backgroundColor = 'lightgray'
-    } else {
-        hoveredElement.style.backgroundColor = '#2b3035'
-    }
-}
-
-function dropdownColorBgReset(element) {
-    element.style.removeProperty('background-color')
+function switchTheme() {
+  let sun = document.getElementById('sun')
+  let moon = document.getElementById('moon')
+  let current_theme = getStoredTheme();
+  if (current_theme == 'light') {
+    setThemeDark();
+    sun.setAttribute('class', 'd-none')
+    moon.removeAttribute('class')
+  } else {
+    setThemeLight();
+    moon.setAttribute('class', 'd-none')
+    sun.removeAttribute('class')
+  }
 }
